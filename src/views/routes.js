8f1014033cfea0a5d8a76b4084ad6ada9469b337
Rick <wong@mollie.com>
2015-09-20 14:25:57 +0200

    Use more common JSX Router notation.

--- src/views/routes.js
+++ src/views/routes.js
@@ -1,8 +1,12 @@
 import Main from "views/Main";
+import React from "react";
+import {Router, Route} from "react-router";
 
 /**
  * The React Router 1.0 routes for both the server and the client.
  */
-export default [
-	{path: "/", component: Main}
-];
+export default (
+	<Router>
+		<Route path="/" component={Main} />
+	</Router>
+);
